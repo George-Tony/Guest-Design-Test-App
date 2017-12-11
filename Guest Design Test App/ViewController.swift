8308d9a829900237787c4ccba67f1e04b821ed75
George-Tony <test@tests-Mac-mini.local>
2017-12-11 23:13:10 +0530

    Auto resizing Tableview cell inside a Scrollview
    Auto resizing Tableview cell inside a Scrollview

--- Guest Design Test App/ViewController.swift
+++ Guest Design Test App/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        return cell
+    }
+    
 
+    override func viewWillAppear(_ animated: Bool) {
+        tableViewOutlet.estimatedRowHeight = 100
+        tableViewOutlet.rowHeight = UITableViewAutomaticDimension
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
